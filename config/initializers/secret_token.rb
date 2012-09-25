6e79389dcf58c4e1a83cfcefa55c4816c43cfb9d
Jose L. Vargas <jlvargass@hotmail.com>
2012-09-25 10:46:57 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hvr::Application.config.secret_token = '8c10575653c579443225d088474011cf517876e330106bb4357fc004f16402eb1f5a83593a6fe6ee771c03e4e1344b4849e45cf1be2ac7836ea0e533b1878d27'
